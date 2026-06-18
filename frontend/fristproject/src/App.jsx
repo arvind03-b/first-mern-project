3e664a240e2a85d36895bcfe8a532e17379ff866
Arvind <arvindbaghel6264@gmail.com>
2026-06-18 15:53:35 +0530

    replace password to commit

--- frontend/fristproject/src/App.jsx
+++ frontend/fristproject/src/App.jsx
@@ -5,7 +5,7 @@ import AddStudent from './pages/add-student';
 function App() {
   return (
     <div style={{ backgroundColor: '#f3f4f6', minHeight: '100vh', padding: '20px 0' }}>
-      <h1> HELLO HOW ARE YOU I am fine and you ?</h1>
+      <h1> HELLO HOW ARE YOU I am fine and you ? replace password to key</h1>
 
       <Home />
 
